c526b2509114317ddc67a23c18b15853e9d3f545
Abdul Hadi <125742871+abdulhadi291@users.noreply.github.com>
2025-05-10 00:10:08 +0500

    assignment 6 Q2

--- assignment6.dart
+++ assignment6.dart
@@ -1,15 +1,30 @@
+// Question 1
+
+// void main() {
+//   int number = 123231456;
+//   int count = 0;
+
+//   if (number == 0) {
+//     count = 1;
+//   } else {
+//     while (number != 0) {
+//       number = number ~/ 10;
+//       count++;
+//     }
+//   }
+
+//   print("Number of digits: $count");
+// }
+
+//-------------------------------------------------------------------------------------
+
+//Question 2
 void main() {
-  int number = 123231456;
-  int count = 0;
+  int number = 5;
+  int i = 1;
 
-  if (number == 0) {
-    count = 1;
-  } else {
-    while (number != 0) {
-      number = number ~/ 10;
-      count++;
-    }
+  while (i <= 10) {
+    print("$number x $i = ${number * i}");
+    i++;
   }
-
-  print("Number of digits: $count");
 }
